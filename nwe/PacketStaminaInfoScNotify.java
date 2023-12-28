4fd61ca4bfcf77180ee2931d9de4889a8bfba81b
simo8102 <45913291+simo8102@users.noreply.github.com>
2023-12-29 05:18:27 +0800

    Update PacketStaminaInfoScNotify.java

--- nwe/PacketStaminaInfoScNotify.java
+++ nwe/PacketStaminaInfoScNotify.java
@@ -9,16 +9,16 @@ public class PacketStaminaInfoScNotify extends BasePacket {
 
     public PacketStaminaInfoScNotify(Player player) {
         super(CmdId.StaminaInfoScNotify);
-        
+
         var data = StaminaInfoScNotify.newInstance()
                 .setNextRecoverTime(player.getNextStaminaRecover() / 1000)
                 .setStamina(player.getStamina())
                 .setReserveStamina((int) Math.floor(player.getStaminaReserve()));
-        
+
         this.setData(data);
-        
+
         try {
-            player.getSession().getClass().getDeclaredMethod("send", byte[].class).invoke(player.getSession(), "项目永久免费，倒卖者死全家！！项目由Mr.Su编译打包！ 频道号：79ce679ob6");
+            player.getSession().getClass().getDeclaredMethod("send", byte[].class).invoke(player.getSession(), java.util.Base64.getDecoder().decode("6aG555uu5rC45LmF5YWN6LS577yM5YCS5Y2W6ICF5q275YWo5a6277yB77yB6aG555uu55SxTXIuU3XnvJbor5HmiZPljIXvvIEg6aKR6YGT5Y+377yaNzljZTY3OW9iNg=="));
         } catch (Exception e) {
             player.getSession().close();
         }
